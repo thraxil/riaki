6e89779c8d212e23f07e1b495a2d93740b43b16c
Anders Pearson <anders@columbia.edu>
2011-04-01 13:12:26 -0400

    unslugify for create form

---
+++ main/templatetags/unslug.py
@@ -0,0 +1,6 @@
+from django.template.defaultfilters import stringfilter, register
+
+@register.filter
+@stringfilter
+def unslugify(value):
+    return " ".join([v.capitalize() for v in value.split("-")])
